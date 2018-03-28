7cf71bfc15ef0015bbf686c25848935e5c0abdff
qffa <nexthop@live.cn>
2018-03-28 12:56:59 +0800

    ch8 update

--- compute.sh
+++ compute.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-echo $m
-
 [[ $1 = "+" ]] && echo "scale=3; $2 + $3" | bc && exit 0
 [[ $1 = "-" ]] && echo "scale=3; $2 - $3" | bc && exit 0
 [[ $1 = "*" ]] && echo "scale=3; $2 * $3" | bc && exit 0
