f646fea2edae2d78b08193809f4e5a73a4e0dfb4
qffa <nexthop@live.cn>
2018-04-07 11:55:11 +0800

    ch14 update

--- ch14_check_port/get.sh
+++ ch14_check_port/get.sh
@@ -4,15 +4,13 @@ socket=`ss -tulnp4 "sport = :$1" | grep $1`
 
 if [[ $? -eq 0 ]]; then
     program=`echo $socket | cut -d\" -f2`
+    if test `which $program`; then
+        which $program
+    else
+        echo "Can't get the process name"
+        exit 2
+    fi
 else
     echo "OFF"
     exit 1
-fi
-
-
-if test `which $program`; then
-    which $program
-else
-    echo "Can't get the process name"
-    exit 2
 fi
